da9fe6dc731dc2262852ffe52db76de59487e620
Swapnil <swapy6031@gmail.com>
2022-11-17 23:48:25 +0530

    done

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
-Class Hello{
-   public static void main (String args[]){
+class Hello{
+   public static void main (String []args){
       System.out.print("Hello to Java");
    }
 }
--- Java.java
+++ Java.java
@@ -1,5 +1,5 @@
-Class Java{
-   public static void main(String args[]){
+class Java{
+   public static void main(String []args){
        System.out.print("Welcome to java");
    }
 }
